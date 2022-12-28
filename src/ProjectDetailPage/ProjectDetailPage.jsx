97a45cd5532c34e2d748884877432084729fae4a
hey-now <rubinand22@gmail.com>
2022-12-28 10:51:34 -0800

    rendering all project details on page

--- src/ProjectDetailPage/ProjectDetailPage.jsx
+++ src/ProjectDetailPage/ProjectDetailPage.jsx
@@ -6,6 +6,26 @@ export default function ProjectDetailPage({ projects }) {
     let project = projects.find((p) => p._id === id);
 
     return(
+        <>
         <h1>{project.projectTitle} Detail</h1>
+        <br />
+        <hr />
+        <br />
+        <div>Created on {new Date(project.createdAt).toLocaleDateString()}</div>
+        <div>Last Update on {new Date(project.updatedAt).toLocaleDateString()}</div>
+        <br />
+        <hr />
+        <br />
+        <div>Description: {project.description}</div>
+        <br />
+        <div>Owner: {project.projectOwner}</div>
+        <br />
+        <div>Project Status: {project.projectStatus}</div>
+        <br />
+        <div>Start Date: {project.projectStartDate}</div>
+        <br />
+        <div>End Date: {project.projectEndDate}</div>
+        <br />
+        </>
     );
 }
